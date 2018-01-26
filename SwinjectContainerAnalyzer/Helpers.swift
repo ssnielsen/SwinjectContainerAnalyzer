639c55ecd4310e1c6e18b0c7dce5d4ddd2276c79
Søren Sønderby Nielsen <ssn@e-conomic.com>
2018-01-26 14:37:28 +0100

    Change xcode project name

--- SwinjectContainerAnalyzer/Helpers.swift
+++ SwinjectContainerAnalyzer/Helpers.swift
@@ -1,6 +1,6 @@
 //
 //  Helpers.swift
-//  ContainerTest
+//  SwinjectContainerAnalyzer
 //
 //  Created by Søren Nielsen on 26/01/2018.
 //  Copyright © 2018 Sonderby Inc. All rights reserved.
--- SwinjectContainerAnalyzer/main.swift
+++ SwinjectContainerAnalyzer/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  ContainerTest
+//  SwinjectContainerAnalyzer
 //
 //  Created by Søren Nielsen on 26/01/2018.
 //  Copyright © 2018 Sonderby Inc. All rights reserved.
