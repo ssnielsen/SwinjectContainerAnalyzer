004be25f5931564e5c0b9bff6dcaea8fe3bb942f
Søren Sønderby Nielsen <ssn@e-conomic.com>
2018-01-26 10:34:11 +0100

    Initial Commit

---
+++ ContainerTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ContainerTest
+//
+//  Created by Søren Nielsen on 26/01/2018.
+//  Copyright © 2018 Sonderby Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
