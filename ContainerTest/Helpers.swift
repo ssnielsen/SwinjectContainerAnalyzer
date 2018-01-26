94d3ce8a9ff3d1dcd33fa28d5c58d3638e885f8e
Søren Sønderby Nielsen <ssn@e-conomic.com>
2018-01-26 13:37:23 +0100

    Initial commit

---
+++ ContainerTest/Helpers.swift
@@ -0,0 +1,100 @@
+//
+//  Helpers.swift
+//  ContainerTest
+//
+//  Created by Søren Nielsen on 26/01/2018.
+//  Copyright © 2018 Sonderby Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    subscript(safeIndex index: Int) -> Element? {
+        if index >= count {
+            return nil
+        }
+
+        return self[index]
+    }
+}
+
+enum ContainerAction {
+    case registration
+    case resolve
+}
+
+private let registerRegex = try! NSRegularExpression(pattern: "\\.register\\((\\w*).self\\)", options: [])
+private let resolveRegex = try! NSRegularExpression(pattern: "\\.resolve\\((\\w*).self\\)", options: [])
+
+struct TypeMatch {
+    let file: String
+    let type: String
+    let line: Int
+    let containerAction: ContainerAction
+}
+
+let containerRegexes = [
+    resolveRegex: ContainerAction.resolve,
+    registerRegex: ContainerAction.registration
+]
+
+typealias TypeAction = (name: String, containerAction: ContainerAction)
+
+func matches(with regexPairs: [NSRegularExpression: ContainerAction],
+             in line: String) -> [TypeAction] {
+    let range = NSRange(location: 0, length: line.count)
+
+    return regexPairs.map { regexPair -> TypeAction? in
+        let regex = regexPair.key
+        let containerAction = regexPair.value
+
+        let registerMatches = regex.matches(in: line, range: range)
+
+        if registerMatches.isEmpty {
+            return nil
+        }
+
+        let match = registerMatches.first!
+        let typeNameRange = match.range(at: 1)
+        let lineNSString = line as NSString
+        let typeName = lineNSString.substring(with: typeNameRange)
+
+        return (name: typeName, containerAction: containerAction)
+    }.flatMap { $0 }
+}
+
+func matches(in files: [String]) -> [TypeMatch] {
+    return files.map { swiftFile -> [TypeMatch] in
+        var inComment = false
+
+        do {
+            let lines = try String(contentsOfFile: swiftFile).components(separatedBy: .newlines)
+            return lines.enumerated().map { object -> [TypeMatch] in
+                let line = object.element.trimmingCharacters(in: .whitespaces)
+
+                if line.hasPrefix("/*") {
+                    inComment = true
+                }
+
+                if line.hasSuffix("*/") {
+                    inComment = false
+                }
+
+                if inComment {
+                    return []
+                }
+
+                if line.hasPrefix("//") {
+                    return []
+                }
+
+                let containerMatches = matches(with: containerRegexes, in: line)
+                return containerMatches.map { match in
+                    return TypeMatch(file: swiftFile, type: match.name, line: object.offset+1, containerAction: match.containerAction)
+                }
+            }.flatMap { $0 }
+        } catch {
+            fatalError("Could not read file at path \(swiftFile)")
+        }
+    }.flatMap { $0 }
+}
--- ContainerTest/main.swift
+++ ContainerTest/main.swift
@@ -7,6 +7,45 @@
 //
 
 import Foundation
+import Darwin
 
-print("Hello, World!")
+guard let path = CommandLine.arguments[safeIndex: 1] else {
+    fatalError("First argument must be the path")
+}
 
+let isStrict = CommandLine.arguments.contains("--strict")
+
+// All files at 'path'
+let files = FileManager.default.enumerator(atPath: path)
+
+// All swift files at 'path'
+let swiftFiles = files?
+    .flatMap { $0 as? String }
+    .filter { $0.hasSuffix(".swift") }
+    .map { "\(path)/\($0)" }
+    ?? []
+
+let containerActions = matches(in: swiftFiles)
+let groupedContainerActions = Dictionary.init(grouping: containerActions, by: { $0.containerAction })
+
+// All container 'registrations' in given swift files
+let registrations = groupedContainerActions[.registration] ?? []
+
+// All container 'resolves' in given swift files
+let resolves = groupedContainerActions[.resolve] ?? []
+
+// Resolves that are not matched with a registration
+let unregisteredResolves = resolves.filter { resolve in
+    !registrations.contains { registration in registration.type == resolve.type }
+}
+
+if unregisteredResolves.isEmpty {
+    exit(0)
+} else {
+    unregisteredResolves.forEach { resolve in
+        // Print to stdout
+        fputs("\(resolve.file):\(resolve.line): warning: Resolve of type \(resolve.type) not registered\n", __stdoutp)
+    }
+
+    exit(isStrict ? 1 : 0)
+}
